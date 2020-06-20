1d723b1a4c63683bd08a53197597c908e5d5a17a
JulietaDA <63409773+JulietaDA@users.noreply.github.com>
2020-06-20 16:14:14 -0300

    Update pert_simulation.py

--- pert_simulation.py
+++ pert_simulation.py
@@ -23,7 +23,7 @@ class ProjectGraph:
         return self.dist_params
 
     def _load_tasks_dist_params(self):
-        # Define los parametros de la distribucion triang para cada tarea.
+        # Define los parametros de la distribucion triangular para cada tarea.
         # Notar que es estatico, pero podria ser dinamico (tomarse de alguna
         # otra fuente). Usa un diccionario.
         d = {}
@@ -51,139 +51,97 @@ class ProjectGraph:
     
 
 def simulate_triangular(op,ml,ps):
-    # TP1 TODO: Completar codigo
     # Esta funcion toma los 3 parametros de la distribucion triangular
     # y devuelve un numero psuedo-aleaotrio con esta distribucion.
-    # Tenemos que adaptar estos parametros a lo que recibe la funcion
-    # triang.ppf para aplicar el metodo de la transformada inversa. .
-
-    # Primero un chequeo: si op == ps, entonces no hay mucho para simular.
-    # Por que? Ya que no se puede generar numeros pseudo aleatorio que tenga el mismo valor para left y right. 
-    if op == ps:
+    
+    if op == ps: # Tenemos en cuenta que si op == ps, no debemos simular nada ya que el valor es el mismo para todos los escenarios.
         return ml
     else:
         return numpy.random.triangular(op, ml, ps)       
 
-    # Esta es la traduccion de parametros a realizar scipy.stats. Revisar la documentaicon
-    # y entender como se representa la distribucion.
-    #loc = op
-    #scale = (ps - op)
-    #c = (ml - loc)/scale
-
-    # Si usan numpy.random, no son necesarias traducciones.
-
 def simulate_tasks_duration(project_graph):
-    # TP1 TODO: Completar codigo
     # Esta funcion el grafo con n tareas, cada cual con sus parametros de la 
     # correspondiente distribucion triangular, y devuelve una lista de 
     # n elementos, donde la posicion i corresponde a la duracion de la i-esima
     # tarea.
-    # Sugerencia: recordar que la distribucion de la tarea i se encuentra en el 
-    # diccionario project_graph.d[i]
-    
-    # Sacar el pass y reemplzar por el codigo
-    ret = []
-    for key, value in project_graph.get_tasks_dist_params().items():
-        ret.append(simulate_triangular(value[0], value[1], value[2]))
-    return ret
+    ret = [] # Creamos una lista vacia. 
+    for key, value in project_graph.get_tasks_dist_params().items(): # Para cada key-value en el diccionario con las estimaciones de duracion de cada tarea...
+        ret.append(simulate_triangular(value[0], value[1], value[2])) # Agrega a la lista el numero aleatorio con dist. triangular que corresponde a cada tarea, 
+                                                                      # teniendo en cuenta las estimaciones de duracion.
+    return ret # Devolve la lista con los numeros aleatorios que corresponden a la duracion calculada con la dist. triangular de cada tarea.
 
 def get_path_duration(path, tasks_times):
-    # TODO TP1: Completar codigo
     # Dado un camino, representado por path y la secuencia de tareas, y la
     # duracion de cada tarea (donde tasks_times[i] es la duracion de la i-esima
-    # tarea, la funcion devuelve la duracion del camino dados esos tiempos.
+    # tarea), la funcion devuelve la duracion del camino dados esos tiempos.
 
-    # Sacar el pass y reemplzar por el codigo
-    duracion_camino = 0
-    for i in path:
-        duracion_camino += tasks_times[i]
-    return duracion_camino
+    duracion_camino = 0 # Inicializamos una variable en 0.
+    for i in path: # Para cada tarea en el camino que se le pasa como arguemento...
+        duracion_camino += tasks_times[i] # Suma en duracion_camino el tiempo que requiere cada tarea dentro del camino.
+    return duracion_camino # Devolve el tiempo total del camino.
     
 
 def get_project_duration(project_graph, tasks_times):
-    # TODO TP1: Completar codigo
     # La duracion del proyecto corresponde al maximo de las duraciones de los caminos.
-    # Recordatorio: los caminos se encuentran en project_graph.paths, y se pueden
-    # obtener con el proyecto get_paths()
+    # La funcion computa la duracion de cada uno de los caminos y retornar el maximo.
 
-    # La funcion debe computar la duracion de cada uno de los caminos y retornar el maximo.
-
-    # Sacar el pass y reemplzar por el codigo
-    duraciones = []
-    for path in project_graph.get_paths():
-        duraciones.append(get_path_duration(path, tasks_times))
-    return max(duraciones)
+    duraciones = [] # Creamos una lista vacia.
+    for path in project_graph.get_paths(): # Para cada camino en la lista de los 6 caminos determinada en el tipo ProjectGraph...
+        duraciones.append(get_path_duration(path, tasks_times)) # Agrega a la lista la duración del camino utilizando la función get_path_duration.
+    return max(duraciones) # Retorna el maximo en duraciones, es decir, el camino que mas tiempo requiere.
     
 
 def simulate(n_sim,project_graph):
-    # TODO TP1: Completar codigo
     # Esta funcion realiza n_sim simulaciones y analiza los resultados necesarios para el
     # arbol de decision.
-    # Idealmente, puede devolver una lista con la duracion para cada una de las
-    # simulaciones para su posterior analisis.
+    # Devuelve una lista con la duracion para cada una de las simulaciones para su posterior analisis.
 
-    # Sacar el pass y reemplzar por el codigo
-    duracion_simulaciones = []
-    for n in range(n_sim):
+    duracion_simulaciones = [] # Creamos una lista vacia. 
+    for n in range(n_sim): # Para n en el rango de cantidad de simulaciones que se pasa como arguemento...
         duracion_simulaciones.append(get_project_duration(project_graph, simulate_tasks_duration(project_graph)))
-    return duracion_simulaciones
+        # Agrega a la lista el tiempo que tarda cada proyecto utilizando la funcion get_project_duration. 
+    return duracion_simulaciones # Devolve la lista con las n_sim simulaciones correspondientes a la duracion de cada proyecto.
         
 
 def get_prob_in_range(vals, a, b):
-    # TODO TP1: Completar codigo
     # Funcion auxiliar para analisis de resultados y calculo de probabilidades.
-    # Dada una muestra de valores vals, calcula la proporcion de x in vals tales que 
+    # Dada una muestra de valores vals, calculamos la proporcion de x in vals tales que 
     # a < x <= b.
 
-    # Sacar el pass y reemplzar por el codigo
-    in_range = [x for x in vals if a < x <= b]
+    in_range = [x for x in vals if a < x <= b] # Agrega a la lista a x para x en vals si x esta en el rango (a,b]
     return len(in_range)/len(vals)
 
 def main():
-    # TODO TP1: Completar codigo
-
     # Fijamos la semilla, por reproducibilidad.
     numpy.random.seed(777) # Nos gusta el 7 :)
-#    random.seed(777)
 
-    # 300k por perder el deadline. Por ahora, 46.0
-    # 150k por terminar en 40 o menos. Por ahora, mantenemos 40.0
-    # Estos son los limites que nos importan.
+    # Definimos:
+    # Limites:
     deadline = 47.0
     incentive = 40.0
-
-    # Primer paso: consideramos el escenario sin contrataciones extra.
-    graph_no_hire = ProjectGraph()
-#    for key, value in graph_no_hire.get_tasks_dist_params().items():
-#        print(simulate_triangular(value[0], value[1], value[2]))
-        
-#    print(graph_no_hire.get_tasks_dist_params())
-
-    # TODO: simular en este contexto!
-# =============================================================================
-#     print(simulate_tasks_duration(graph_no_hire), end = "\n")
-#     print(get_path_duration(graph_no_hire.get_paths()[0], simulate_tasks_duration(graph_no_hire)))
-#     print(get_project_duration(graph_no_hire, simulate_tasks_duration(graph_no_hire)))
-#     print(simulate(1000, graph_no_hire))
-# =============================================================================
-    simulacion_no_hire = simulate(1000, graph_no_hire)
     
-    # Grafico de la distribucion de las simulaciones:
-    plt.hist(simulacion_no_hire,density=True, color = "pink") 
-    plt.xlabel("Sin contratación")
-    
-    plt.show()
-    
-    # TODO: Analizar los resultados.
+    # Posibles resultados:
     ganancia = 5000000
     inversion = -100000
     incentivo = 150000
     penalizacion = -300000
+
+    # Primer paso: consideramos el escenario sin contrataciones extra.
+    graph_no_hire = ProjectGraph() 
+
+    simulacion_no_hire = simulate(1000, graph_no_hire) # Definimos las 1.000 simulaciones cuando no hay contrataciones extra.
     
-    prob_menor_igual_cuarenta_no_hire = get_prob_in_range(simulacion_no_hire, 0, incentive)
-    prob_cuarenta_cuarentaysiete_no_hire = get_prob_in_range(simulacion_no_hire, incentive, deadline)
-    prob_mas_cuarentaysiete_no_hire = get_prob_in_range(simulacion_no_hire, deadline, 100)
+    # Grafico de la distribucion de las simulaciones:
+    plt.hist(simulacion_no_hire,density=True, color = "pink") 
+    plt.xlabel("Sin contratación")    
+    plt.show()
     
+    # Analizamos los resultados:
+    prob_menor_igual_cuarenta_no_hire = get_prob_in_range(simulacion_no_hire, 0, incentive) # Probabilidad de terminar en 40 semanas o menos.
+    prob_cuarenta_cuarentaysiete_no_hire = get_prob_in_range(simulacion_no_hire, incentive, deadline) # Probabilidad de terminar entre la semana 40 y 47.
+    prob_mas_cuarentaysiete_no_hire = get_prob_in_range(simulacion_no_hire, deadline, 100) # Probabilidad de terminar despues de la semana 47.
+    
+    # Imprimimos por pantalla los resultados:
     print("Sin contratación:")
     print("Probabilidad  de terminar en la semana 40 o antes: ", prob_menor_igual_cuarenta_no_hire)
     print("Probabilidad  de terminar entre la semana 40 y 47: ", prob_cuarenta_cuarentaysiete_no_hire)
@@ -191,45 +149,54 @@ def main():
   
     # Tabla de pagos sin contrataciones:
     matriz_no_hire = [(ganancia + incentivo), (ganancia), (ganancia + penalizacion)]
-        
+    
+    # Tabla de probabilidades:    
     p_no_hire = [prob_menor_igual_cuarenta_no_hire,
                  prob_cuarenta_cuarentaysiete_no_hire,
                  prob_mas_cuarentaysiete_no_hire]
-   
-    print("Ganancia total:", numpy.dot(matriz_no_hire, p_no_hire))     
+    
+    # Imprimimos por pantalla la ganancia total sin contratacion:
+    print("Ganancia total:", numpy.dot(matriz_no_hire, p_no_hire)) # Hacemos sumaproducto entre las matrices.
     print()
 
-    # Segundo paso: analizar en el contexto que se hace la contratacion.
+    # Segundo paso: consideramos el escenario con contratacion extra.
     # Esto lo representamos modificando la duracion de las tarea 2.
     graph_hire = graph_no_hire
     graph_hire.dist_params[2] = (6.0, 9.0, 13.0)
-
-    # TODO: simular en este contexto!
-    simulacion_hire = simulate(1000, graph_hire)
+    
+    simulacion_hire = simulate(1000, graph_hire) # Definimos las 1.000 simulaciones cuando hay contrataciones extra.
     
     # Grafico de la distribucion de las simulaciones:
     plt.hist(simulacion_hire,density=True, color = "grey")
     plt.xlabel("Con contratación")
     plt.show()
     
-    # TODO: Analizar los resultados.
-    # Tabla de pagos con contrataciones:
-    prob_menor_igual_cuarenta_hire = get_prob_in_range(simulacion_hire, 0, incentive)
-    prob_cuarenta_cuarentaysiete_hire = get_prob_in_range(simulacion_hire, incentive, deadline)
-    prob_mas_cuarentaysiete_hire = get_prob_in_range(simulacion_hire, deadline, 100)
+    # Grafico con las dist. de cuando hay contratacion extra y cuando no:
+    plt.hist(simulacion_no_hire,density=True, color = "pink") 
+    plt.hist(simulacion_hire,density=True, color = "grey")
+    plt.show()
     
+    # Analizamos los resultados.
+    prob_menor_igual_cuarenta_hire = get_prob_in_range(simulacion_hire, 0, incentive) # Probabilidad de terminar en 40 semanas o menos.
+    prob_cuarenta_cuarentaysiete_hire = get_prob_in_range(simulacion_hire, incentive, deadline) # Probabilidad de terminar entre la semana 40 y 47.
+    prob_mas_cuarentaysiete_hire = get_prob_in_range(simulacion_hire, deadline, 100) # Probabilidad de terminar despues de la semana 47.
+    
+    # Imprimimos por pantalla los resultados:
     print("Con contratación:")
     print("Probabilidad  de terminar en la semana 40 o antes: ", prob_menor_igual_cuarenta_hire)
     print("Probabilidad  de terminar entre la semana 40 y 47: ", prob_cuarenta_cuarentaysiete_hire)
     print("Probabilidad  de terminar luego de la semana 47: ", prob_mas_cuarentaysiete_hire)
-
+    
+    # Tabla de pagos con contrataciones:
     matriz_hire = [(ganancia + incentivo + inversion), (ganancia + inversion), (ganancia + penalizacion + inversion)]
-        
+    
+    # Tabla de probabilidades:    
     p_hire = [prob_menor_igual_cuarenta_hire,
               prob_cuarenta_cuarentaysiete_hire,
               prob_mas_cuarentaysiete_hire]
     
-    print("Ganancia total:", numpy.dot(matriz_hire, p_hire))   
+    # Imprimimos por pantalla la ganancia total con contratacion:
+    print("Ganancia total:", numpy.dot(matriz_hire, p_hire)) # Hacemos sumaproducto entre las matrices.
     print()
 
     # Resultado final: Decisión
